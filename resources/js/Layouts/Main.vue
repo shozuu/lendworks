bdf3f508985e721712f6146ddd8165e825919d10
Allen Tan <tanallenbernard@gmail.com>
2024-11-03 10:16:18 +0800

    implement the switchTheme on-click of the icon and add classes

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -1,13 +1,22 @@
-<script setup></script>
+<script setup>
+    import { switchTheme } from '../theme';
+</script>
 
 <template>
-    <header class="bg-slate-800 text-white">
-        <nav class="p-6 mx-auto max-w-screen-lg">
-            <Link :href="route('home')">Home</Link>
+    <header>
+        <nav class="p-6 mx-auto max-w-screen-lg flex items-center justify-between">
+            <Link :href="route('home')" class=" font-semibold text-xl">Lend<span class=" text-primary">Works</span></Link>
+            
+            <div>
+                <button 
+                @click="switchTheme">
+                    <i class="fa-solid fa-circle-half-stroke"></i>
+                </button>
+            </div>
         </nav>
     </header>
 
-    <main class="p-6">
+    <main class="p-6 mx-auto max-w-screen-lg">
         <slot />
     </main>
 </template>
