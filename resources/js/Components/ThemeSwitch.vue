6d059575d8d2f945136f7a9edf3bbab017557d50
Allen Tan <tanallenbernard@gmail.com>
2024-11-07 22:33:07 +0800

    create component for theme switch icon

---
+++ resources/js/Components/ThemeSwitch.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+
+<template>
+    <div
+        class="flex items-center w-full px-4 py-2 text-sm rounded-md hover:bg-secondary-hover text-left cursor-pointer select-none"
+        >Dark Mode
+        <button
+            class="text-text focus:outline-none ml-auto"
+        >
+            <i class="fa-solid fa-circle-half-stroke"></i>
+        </button>
+    </div>
+</template>
