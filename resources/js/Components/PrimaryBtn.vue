4b6b430df2c57c6be003c00b3b74fc7398abb4a9
moeweyyy <marieandreeei.pczc@gmail.com>
2024-11-08 01:25:07 +0800

    create primary button component to be used in login/register forms

---
+++ resources/js/Components/PrimaryBtn.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <button class="px-6 py-2 rounded-lg bg-primary hover:bg-primary-hover disabled:bg-disabled disabled:cursor-wait">
+        <slot />
+    </button>
+</template>
