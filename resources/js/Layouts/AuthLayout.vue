ce6e1654407ae67b9500425956e8b88856661793
Allen Tan <tanallenbernard@gmail.com>
2024-11-07 22:21:30 +0800

    create separate layout that will handle login and register views

---
+++ resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import Logo from '../Components/Logo.vue';   
+</script>
+
+<template>
+    <div class="flex flex-col items-center my-20">
+        <div class=" h-14 shrink-0">
+            <Logo :wrenchColor="'var(--wrench-color)'" :handColor="'var(--hand-color)'" />
+        </div>
+    </div>
+    <slot />
+</template>
