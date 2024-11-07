565be0642cc45347490be9e5428ce5eaf7f18d34
stevegwapss <steventumimbang@gmail.com>
2024-11-08 02:31:44 +0800

    Create profile drop down and item

---
+++ resources/js/Components/ProfileDropdownItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+    defineProps({
+        routeName: String,
+        componentName: String
+    });
+</script>
+
+<template>
+    <Link 
+        :href="route(routeName)"
+        class="block w-full px-4 py-2 text-sm rounded-md hover:bg-secondary text-left"><slot />
+    </Link>
+</template>
