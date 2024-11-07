c78b733bf463c5792a9f7bf4c071d88329ac0fbb
stevegwapss <steventumimbang@gmail.com>
2024-11-08 02:35:12 +0800

    Create dropdown profile icon

---
+++ resources/js/Components/ProfileIcon.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <button
+        type="button" 
+        class="rounded-full focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-background transition-transform duration-200 hover:scale-105">
+        <slot />
+    </button>
+</template>
