00fa9f855819b8133c100cbaa0bbce8b157a55b0
Allen Tan <tanallenbernard@gmail.com>
2024-11-09 18:23:46 +0800

    use shadcn-vue and modify files to accommodate changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,96 @@
+const animate = require("tailwindcss-animate")
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
+  safelist: ["dark"],
+  prefix: "",
+  
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.{js,jsx,vue}",
   ],
-  darkMode: 'class',
+  
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         Poppins: ['Poppins', 'sans-serif'],
       },
       colors: {
-        sides: 'var(--sides-color)',
-        background: 'var(--background-color)',
-        text: 'var(--text-color)',
-        'text-muted': 'var(--text-muted)',
-        primary: 'var(--primary-color)',
-        'primary-hover': 'var(--primary-hover-color)',
-        secondary: 'var(--secondary-color)',
-        'secondary-hover': 'var(--secondary-hover-color)',
-        accent: 'var(--accent-color)',
-        'accent-hover': 'var(--accent-hover-color)',
-        warning: 'var(--warning-color)',
-        'warning-hover': 'var(--warning-hover-color)',
-        success: 'var(--success-color)',
-        'success-hover': 'var(--success-hover-color)',
-        border: 'var(--border-color)',
-        'border-hover': 'var(--border-color-hover)',
-        disabled: 'var(--disabled-color)',
-        'disabled-text': 'var(--disabled-text-color)',
-        focus: 'var(--focus-color)',
-        card: 'var(--card-color)',
-        'card-hover': 'var(--card-hover-color)',
-        'input-background': 'var(--input-background-color)',
-        'input-border': 'var(--input-border-color)',
-        'input-focus-border': 'var(--input-focus-border-color)',
-        'wrench-color': 'var(--wrench-color)',
-        'hand-color': 'var(--hand-color)',
-      }
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        "collapsible-down": {
+          from: { height: 0 },
+          to: { height: 'var(--radix-collapsible-content-height)' },
+        },
+        "collapsible-up": {
+          from: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-in-out",
+        "collapsible-up": "collapsible-up 0.2s ease-in-out",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [animate],
 }
-
