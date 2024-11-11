e69cb73af44ea11bd80995445493e374a64af1fa
Allen Tan <tanallenbernard@gmail.com>
2024-11-12 00:42:37 +0800

    create controller to handle routes of email verification

---
+++ app/Http/Controllers/Auth/EmailVerificationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class EmailVerificationController extends Controller
+{
+
+}
