0715ce0e3a203b3ea27fc0cf291492e7ad9d2a05
Allen Tan <tanallenbernard@gmail.com>
2024-11-12 00:48:15 +0800

    define routes for each action of email verification

--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticateController;
+use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +20,11 @@ Route::middleware('guest')->group(function() {
 Route::middleware('auth')->group(function() {
     // logout
     Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout');
+
+    // email verification notice can be triggered if the user tries to access other features of the website. this logic can also be used to fully verify user in the future (other credentials/documents)
+    Route::get('/email/verify', [EmailVerificationController::class, 'notice'])->name('verification.notice');
+
+    Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'handler'])->middleware('signed')->name('verification.verify');
+
+    Route::post('/email/verification-notification', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1')->name('verification.send');
 });
