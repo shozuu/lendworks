ec12b42594ead7fe99d70dad182b954cefc0d093
Allen Tan <tanallenbernard@gmail.com>
2024-11-12 00:48:54 +0800

    use alert component for resend verification

---
+++ resources/js/Components/ui/alert/index.js
@@ -0,0 +1,28 @@
+import { cva } from 'class-variance-authority';
+
+export { default as Alert } from './Alert.vue';
+export { default as AlertDescription } from './AlertDescription.vue';
+export { default as AlertTitle } from './AlertTitle.vue';
+
+export const alertVariants = cva(
+  'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
+  {
+    variants: {
+      variant: {
+        default: 'bg-background text-foreground',
+        destructive:
+          'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+        success:
+          'border-green-500 text-green-700 dark:bg-green-700/10 dark:border-green-700 dark:text-green-200 [&>svg]:text-green-500',
+        warning:
+          'border-yellow-500 text-yellow-700 dark:bg-yellow-700/10 dark:border-yellow-700 dark:text-yellow-200 [&>svg]:text-yellow-500',
+        info:
+          'border-blue-500 text-blue-700 dark:bg-blue-700/10 dark:border-blue-700 dark:text-blue-200 [&>svg]:text-blue-500',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
