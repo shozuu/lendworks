13693a30b4b0c11a01c0ef145893f6db73f3fe81
Allen Tan <tanallenbernard@gmail.com>
2024-11-12 00:50:13 +0800

    use 'verified' middleware in home route for the meantime

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Home')->name('home');
+// 'verified' middleware can be used to protect routes that need full user verification but for now, i'll put it at home to demonstrate functionality
+Route::inertia('/', 'Home')->middleware('verified')->name('home');
+
 Route::inertia('/explore', 'Explore')->name('explore');
 Route::inertia('/my-rentals', 'MyRentals')->name('my-rentals');
 
