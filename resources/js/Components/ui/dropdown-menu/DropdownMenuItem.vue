a66e5c27280b121ccc457ed980e48e44183163f6
Allen Tan <tanallenbernard@gmail.com>
2024-11-11 16:27:38 +0800

    install and use shadcn-vue components

---
+++ resources/js/Components/ui/dropdown-menu/DropdownMenuItem.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { cn } from '@/lib/utils';
+import { DropdownMenuItem, useForwardProps } from 'radix-vue';
+import { computed } from 'vue';
+
+const props = defineProps({
+  disabled: { type: Boolean, required: false },
+  textValue: { type: String, required: false },
+  routeName: { type: String, required: false },
+  method: { type: String, default: "get" },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+  inset: { type: Boolean, required: false },
+});
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <DropdownMenuItem
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'relative flex cursor-pointer select-none items-center rounded-sm gap-2 px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50  [&>svg]:size-4 [&>svg]:shrink-0',
+        inset && 'pl-8',
+        props.class,
+      )
+    "
+  >
+    <Link 
+      :href="route(routeName)"
+      :method="method"
+      :as="method === 'post' ? 'button' : 'a'"
+      :class="method === 'post' ? 'w-full' : ''"
+    >
+      <slot/>
+    </Link>
+  </DropdownMenuItem>
+</template>
+
