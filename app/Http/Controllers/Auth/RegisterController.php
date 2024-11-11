f3668411c687d1663e6ca539250757c12e977333
Allen Tan <tanallenbernard@gmail.com>
2024-11-12 00:45:18 +0800

    use event to verify email upon registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -24,6 +25,7 @@ class RegisterController extends Controller
         $user = User::create($credentials);
 
         // send verification email
+        event(new Registered($user));
 
         Auth::login($user); 
         
