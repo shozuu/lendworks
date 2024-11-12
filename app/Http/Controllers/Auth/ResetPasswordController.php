75c785e1f7949e4652dd5b9e8870bd2a9a7138dd
Allen Tan <tanallenbernard@gmail.com>
2024-11-12 18:31:08 +0800

    create function to handle sending reset pw link to email

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
     public function requestPass() {
-        Inertia::render('Auth/ForgotPassword');
+        return Inertia::render('Auth/ForgotPassword', [
+            'status' => session('status')
+        ]);
     }
+    
+    public function sendEmail(Request $request) {
+        $request->validate(['email' => 'required|email']);
+         
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+        
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)]) // success
+            : back()->withErrors(['email' => __($status)]); // failure
+    }
+
+
 }
