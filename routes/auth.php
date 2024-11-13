9e70331edb9cb371927881b6d8848cbd2e585e4d
Allen Tan <tanallenbernard@gmail.com>
2024-11-13 22:23:24 +0800

    fix format

--- routes/auth.php
+++ routes/auth.php
@@ -19,11 +19,8 @@ Route::middleware('guest')->group(function() {
 
     // reset password
     Route::get('/forgot-password', [ResetPasswordController::class, 'requestPass'])->name('password.request');
-
     Route::post('/forgot-password', [ResetPasswordController::class, 'sendEmail'])->name('password.email');
-
     Route::get('/reset-password/{token}', [ResetPasswordController::class, 'resetForm'])->name('password.reset');
-
     Route::post('/reset-password', [ResetPasswordController::class, 'resetHandler'])->name('password.update');
 });
 
@@ -33,18 +30,14 @@ Route::middleware('auth')->group(function() {
     Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout'); 
 
     // by using password.confirm as middleware, we enable the password confirmation routes that confirm user's password before accessing a page/view
-
     // notice can be triggered if the user tries to access other features of the website. this logic can also be used to fully verify user in the future (other credentials/documents)
 
     // email verification 
     Route::get('/email/verify', [EmailVerificationController::class, 'notice'])->name('verification.notice');
-
     Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'handler'])->middleware('signed')->name('verification.verify');
-
     Route::post('/email/verification-notification', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1')->name('verification.send');
 
     // password confirmation
     Route::get('/confirm-password', [ConfirmPasswordController::class, 'create'])->name('password.confirm');
-
     Route::post('/confirm-password', [ConfirmPasswordController::class, 'store'])->middleware('throttle:6,1')->name('password.confirm');
 });
