a6e722a6989ee70939e60cedc0bc5bcd028493b5
Allen Tan <tanallenbernard@gmail.com>
2024-11-14 00:18:31 +0800

    fix routing problem

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function() {
-    // home
-    Route::get('/', [HomeController::class, 'index'])->name('home');
-
-    // explore
-    Route::inertia('/explore', 'Explore')->name('explore');
-
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::inertia('/explore', 'Explore')->name('explore');
 
 // 'verified' and password.confirm middleware can be used to protect routes that need full user verification but for now, i'll put it at my rentals to demonstrate functionality
 
