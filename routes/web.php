d43e3a2cdbb28376f0b85b86b3b77ef9e1bf69a2
Allen Tan <tanallenbernard@gmail.com>
2024-11-13 22:23:06 +0800

    group middleware for other routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// 'verified' middleware can be used to protect routes that need full user verification but for now, i'll put it at home to demonstrate functionality
-Route::inertia('/', 'Home')->middleware('verified')->name('home');
+Route::middleware('guest')->group(function() {
+    // home
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::inertia('/explore', 'Explore')->name('explore');
-Route::inertia('/my-rentals', 'MyRentals')->name('my-rentals');
+    // explore
+    Route::inertia('/explore', 'Explore')->name('explore');
+
+});
+
+// 'verified' and password.confirm middleware can be used to protect routes that need full user verification but for now, i'll put it at my rentals to demonstrate functionality
+
+Route::middleware(['auth'])->group(function() {
+    // profile
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');   
+    Route::patch('/profile', [ProfileController::class, 'updateInfo'])->name('profile.info');
+
+    // my rentals
+    Route::inertia('/my-rentals', 'MyRentals')->middleware('verified')->name('my-rentals');
+});
 
 require __DIR__ . '/auth.php';
