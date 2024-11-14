f2b2a1fafd3fb85c0b2828cdbcc01ac113db7d7f
Allen Tan <tanallenbernard@gmail.com>
2024-11-14 20:39:58 +0800

    create view to take user input for updating password

---
+++ resources/js/Pages/Profile/Sections/UpdatePassword.vue
@@ -0,0 +1,82 @@
+<script setup>
+    import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+    import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+    import InputField from '../../../Components/InputField.vue'
+    import { Button } from '@/components/ui/button'
+    import { useForm } from '@inertiajs/vue3'
+    import { KeyRound  } from 'lucide-vue-next'
+
+    const form = useForm({
+        current_password: '',
+        password: '',
+        password_confirmation: ''
+    })
+
+    const submit = () => {
+        form.put(route('profile.password'), {
+            onSuccess: () => form.reset(),
+            onError: () => form.reset(),
+            preserveScroll: true,
+        })
+    }
+</script>
+
+<template>
+    <Card>
+        <CardHeader>
+            <CardTitle>Update Password</CardTitle>
+            <CardDescription class="w-full lg:w-1/2">Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.</CardDescription>
+        </CardHeader>
+        
+        <CardContent>
+
+            <Alert 
+                v-if="form.recentlySuccessful === true" 
+                variant="success" class="w-full lg:w-1/2 mb-5">
+                <KeyRound class="shrink-0"/>
+                <div>
+                    <AlertTitle>Success!</AlertTitle>
+                    <AlertDescription>
+                        <p>Your password has been successfully updated.</p>
+                    </AlertDescription>
+                </div>
+            </Alert>
+
+            <form @submit.prevent="submit" class="space-y-6">
+                <InputField 
+                    label="Current Password" 
+                    icon="key" 
+                    class="w-full lg:w-1/2" 
+                    type="password"
+                    v-model="form.current_password"
+                    :error="form.errors.current_password"
+                />
+
+                <InputField 
+                    label="New Password" 
+                    icon="key" 
+                    class="w-full lg:w-1/2" 
+                    type="password"
+                    v-model="form.password"
+                    :error="form.errors.password"
+                />
+
+                <InputField 
+                    label="Confirm New Password" 
+                    icon="key" 
+                    class="w-full lg:w-1/2" 
+                    type="password"
+                    v-model="form.password_confirmation"
+                    :error="form.errors.password"
+                />
+
+                <Button
+                    :disabled="form.processing"
+                    class="">
+                    Save
+                </Button>
+            </form>
+            
+        </CardContent>
+    </Card>
+</template>
