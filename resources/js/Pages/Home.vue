4855d6a26d1639831a1f0dbd9899536cc80c5876
Allen Tan <tanallenbernard@gmail.com>
2024-11-18 01:13:32 +0800

    add cta and grid for listings

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,43 @@
-<script setup></script>
+<script setup>
+import ItemCard from "@/Components/ItemCard.vue";
+import { Separator } from "@/components/ui/separator";
+import CTAHome from "@/Components/CTAHome.vue";
+
+defineProps({
+	listings: Object,
+	CTAImage: String,
+});
+</script>
 
 <template>
-    <Head title="| Home"/>
-    <div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis aperiam quae modi officia, repudiandae voluptate vero error tempora nobis ex reprehenderit omnis, expedita alias, in reiciendis natus distinctio architecto eligendi.</p>
-    </div>
+	<Head title="| Home" />
+
+	<div class="space-y-12">
+		<!-- CTA -->
+		<CTAHome :CTAImage="CTAImage" />
+
+		<!-- popular tools -->
+		<div>
+			<div class="flex items-center justify-between">
+				<div class="space-y-1">
+					<h2 class="text-2xl font-semibold tracking-tight">Rent Now</h2>
+					<p class="text-sm text-muted-foreground">
+						Top-rated tools, handpicked for your projects. Available today!
+					</p>
+				</div>
+			</div>
+
+			<Separator class="my-4" />
+
+			<div v-if="Object.keys(listings.data).length">
+				<div
+					class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+				>
+					<div v-for="listing in listings.data" :key="listing.id">
+						<ItemCard :listing="listing" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
