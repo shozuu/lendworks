5bbafdb1e9640c1930b61b6afd67229622ca9bec
stevegwapss <steventumimbang@gmail.com>
2024-11-20 23:38:26 +0800

    create function in controller to handle search query

---
+++ app/Http/Controllers/ExploreController.php
@@ -0,0 +1,30 @@
+
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ExploreController extends Controller
+{
+    public function index(Request $request)
+    {
+        $listings = Listing::whereHas('user', function(Builder $query) {
+            $query->where('role', '!=', 'suspended');
+        })
+            ->with('user')
+            ->where('approved', true)
+            ->filter(request( ['search'] )) // or [$request->search]
+            ->latest() // sort by created_at
+            ->paginate(20)
+            ->withQueryString(); // retains search query when navigating paginated links
+
+        return Inertia::render('Explore', [
+            'listings' => $listings,
+            'searchTerm' => $request->search,
+        ]);
+    }
+}
