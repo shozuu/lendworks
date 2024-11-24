1aff77e126850becbd39123946ec6a5264d54103
Allen Tan <tanallenbernard@gmail.com>
2024-11-25 00:37:58 +0800

    use form components

---
+++ resources/js/Components/ui/textarea/Textarea.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { cn } from "@/lib/utils";
+import { useVModel } from "@vueuse/core";
+
+const props = defineProps({
+	class: { type: null, required: false },
+	defaultValue: { type: [String, Number], required: false },
+	modelValue: { type: [String, Number], required: false },
+});
+
+const emits = defineEmits(["update:modelValue"]);
+
+const modelValue = useVModel(props, "modelValue", emits, {
+	passive: true,
+	defaultValue: props.defaultValue,
+});
+</script>
+
+<template>
+	<textarea
+		v-model="modelValue"
+		:class="
+			cn(
+				'flex min-h-20 w-full focus:ring-transparent focus:border-primary rounded-md border border-input bg-background px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+				props.class
+			)
+		"
+	/>
+</template>
