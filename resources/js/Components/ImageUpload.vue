5e22487526438a38001482249558f61b73c367cc
Allen Tan <tanallenbernard@gmail.com>
2024-11-25 00:59:29 +0800

    create imageUpload component that uses filepond

---
+++ resources/js/Components/ImageUpload.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from "vue";
+import vueFilePond from "vue-filepond";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
+import "filepond/dist/filepond.min.css";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+
+// register plugins to create & use FilePond component
+const FilePond = vueFilePond(
+	FilePondPluginImagePreview,
+	FilePondPluginFileValidateType,
+	FilePondPluginFileValidateSize
+);
+
+const emit = defineEmits(["images"]);
+const files = ref([]);
+
+const onUpdateFiles = (fileItems) => {
+	console.log(fileItems);
+	const imagesArray = fileItems.map(
+		(item) => item.file
+		// console.log(item.file);
+	);
+	console.log(imagesArray);
+	emit("images", imagesArray);
+};
+</script>
+<template>
+	<FilePond
+		:files="files"
+		allow-multiple="true"
+		allow-reorder="true"
+		allow-remove="true"
+		accepted-file-types="image/jpeg, image/png, image/jpg"
+		max-file-size="3MB"
+		label-idle="Drag & Drop your files or <span class='filepond--label-action'>Browse</span>"
+		@updatefiles="onUpdateFiles"
+		class="bg-background text-foreground"
+	/>
+</template>
