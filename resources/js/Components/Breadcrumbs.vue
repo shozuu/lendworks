57079a264d25ec088c192a5e51747a2318db34d6
moeweyyy <marieandreeei.pczc@gmail.com>
2024-11-29 01:03:40 +0800

    create breadcrumb for show view

---
+++ resources/js/Components/Breadcrumbs.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+defineProps({ category: String, currentPage: String });
+</script>
+
+<template>
+	<Breadcrumb>
+		<BreadcrumbList>
+			<BreadcrumbItem>
+				<BreadcrumbLink :href="route('home')"> Home </BreadcrumbLink>
+			</BreadcrumbItem>
+
+			<BreadcrumbSeparator />
+
+			<BreadcrumbItem>
+				<BreadcrumbLink :href="route('explore')"> Explore </BreadcrumbLink>
+			</BreadcrumbItem>
+
+			<BreadcrumbSeparator />
+
+			<BreadcrumbItem>
+				<BreadcrumbLink :href="route('explore')"> {{ category }} </BreadcrumbLink>
+			</BreadcrumbItem>
+
+			<BreadcrumbSeparator />
+
+			<BreadcrumbItem>
+				<BreadcrumbPage>{{ currentPage }}</BreadcrumbPage>
+			</BreadcrumbItem>
+		</BreadcrumbList>
+	</Breadcrumb>
+</template>
