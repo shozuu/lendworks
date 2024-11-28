e43e5af4da18ef92a451d052fb7a43b09cd1b7bc
Alfaisal M. Sappari <alsappari14@gmail.com>
2024-11-29 00:18:54 +0800

    make show route accessible to guest user

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ListingController::class, 'index'])->name('home');
 Route::get('/explore', [ExploreController::class, 'index'])->name('explore');
+Route::get('listing/{listing}', [ListingController::class, 'show'])->name('listing.show');
 
 
 // 'verified' and password.confirm middleware can be used to protect routes that need full user verification but for now, i'll put it at my rentals to demonstrate functionality
@@ -23,7 +24,7 @@ Route::middleware(['auth'])->group(function() {
     Route::inertia('/my-rentals', 'MyRentals')->middleware('verified')->name('my-rentals');
 
     // listing crud
-    Route::resource('listing', ListingController::class)->except('index')->middleware('verified');
+    Route::resource('listing', ListingController::class)->except(['index', 'show'])->middleware('verified');
 
 });
 
