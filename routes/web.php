13703d34e4c7a51a9990cbc2197b3abe263c68e7
moeweyyy <marieandreeei.pczc@gmail.com>
2024-11-29 01:17:55 +0800

    fix route hierarchy error

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,6 @@ use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ListingController::class, 'index'])->name('home');
-Route::get('/explore', [ExploreController::class, 'index'])->name('explore');
-
-Route::resource('listing', ListingController::class)->except('index');
-
 // 'verified' and password.confirm middleware can be used to protect routes that need full user verification but for now, i'll put it at my rentals to demonstrate functionality
 
 Route::middleware(['auth'])->group(function() {
@@ -22,6 +17,13 @@ Route::middleware(['auth'])->group(function() {
 
     // my rentals
     Route::inertia('/my-rentals', 'MyRentals')->middleware('verified')->name('my-rentals');
+
+    // listing crud 
+    Route::resource('listing', ListingController::class)->except(['index', 'show'])->middleware('verified');
 });
+
+Route::get('/', [ListingController::class, 'index'])->name('home');
+Route::get('listing/{listing}', [ListingController::class, 'show'])->name('listing.show');
+Route::get('/explore', [ExploreController::class, 'index'])->name('explore');
  
 require __DIR__ . '/auth.php';
