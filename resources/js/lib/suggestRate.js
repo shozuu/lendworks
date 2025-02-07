7be396becacb3b6d1b30c49d8ce7dc16b4eeb6bd
Allen Tan <tanallenbernard@gmail.com>
2025-02-07 11:44:22 +0800

    add function to calculate and suggest deposit rates based on item value

--- resources/js/lib/suggestRate.js
+++ resources/js/lib/suggestRate.js
@@ -25,3 +25,25 @@ export function calculateDailyRate(itemValue) {
 
     return { minRate, maxRate };
 }
+
+export function calculateDepositFee(itemValue) {
+    const category = getValueCategory(itemValue);
+    
+    let minRate, maxRate;
+    switch (category) {
+        case 'low-value':
+            minRate = itemValue * 0.20;  // 20% of item value
+            maxRate = itemValue * 0.30;  // 30% of item value
+            break;
+        case 'mid-value':
+            minRate = itemValue * 0.25;  // 25% of item value
+            maxRate = itemValue * 0.35;  // 35% of item value
+            break;
+        case 'high-value':
+            minRate = itemValue * 0.30;  // 30% of item value
+            maxRate = itemValue * 0.40;  // 40% of item value
+            break;
+    }
+
+    return { minRate, maxRate };
+}
